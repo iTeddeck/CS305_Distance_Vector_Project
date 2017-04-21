c3262b08ef70cfebf0f5631496a3a37ef8bc3dde
minahanjlaptop <minahanj@lafayette.edu>
2017-04-20 20:58:29 -0400

    started on listener thread

--- root/ListenerThread.java
+++ root/ListenerThread.java
@@ -1,13 +1,33 @@
+import java.io.*;
+import java.net.*;
 
 public class ListenerThread implements Runnable {
     private int portNum;
-
+    DatagramSocket serverSocket;
+    byte[] receiveData = new byte[1024];
     public ListenerThread(int portNum) {
         this.portNum = portNum;
         //Create UDP Connection
+        try {
+            serverSocket = new DatagramSocket(portNum);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
+        while(true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            try {
+                serverSocket.receive(receivePacket);
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+
+                String message = new String(receivePacket.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }

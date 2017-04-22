bbddde282b05c1e4d481e9d763c734ad6670472c
minahanjlaptop <minahanj@lafayette.edu>
2017-04-22 15:22:58 -0400

    kind of regressed...  data sockets working though??

--- root/CommandThread.java
+++ root/CommandThread.java
@@ -5,54 +5,55 @@ public class CommandThread implements Runnable {
 
     RoutingTable rTable;
     NeighborTable nTable;
-
     public CommandThread(RoutingTable rTable) {
         this.rTable = rTable;
         this.nTable = nTable;
     }
 
     public void run() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line = "";
-        try {
-            line = reader.readLine();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        //while line is not empty
-        while( line != null && !line.equals("") )
-        {
-            if(line.contains("MSG")) {
-
-            } else if(line.contains("PRINT")) {
-                System.out.print("                ");
-                for(int a = 0; a < rTable.outwardIP.size();a++) {
-                    for(int i = 0; i < rTable.outwardIP.get(a).size(); i++) {
-                        System.out.print(rTable.outwardIP.get(a).get(i).getIP() + "," + rTable.outwardIP.get(a).get(i).getPort() + "||");
-                    }
-                }
-                System.out.println();
-
-                for(int i = 0; i < rTable.costToGet.size(); i++) {
-                    System.out.print(rTable.neighborAddresses.get(i).getIP() + "," + rTable.neighborAddresses.get(i).getPort() + "||");
-                    for(int j = 0; j < rTable.costToGet.get(i).size(); j++) {
-                        System.out.print(rTable.costToGet.get(i).get(j) + "               ");
-                    }
-                    System.out.println();
-                }                
-            } else if(line.contains("CHANGE")) {
-
-            } else {
-                System.out.println("Command does not exist");
-            }
-
+        if(true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String line = "";
             try {
                 line = reader.readLine();
             } catch (Exception e) {
                 System.out.println(e);
             }
+
+            //while line is not empty
+            while( line != null && !line.equals(""))
+            {
+                if(line.contains("MSG")) {
+
+                } else if(line.contains("PRINT")) {
+                    System.out.print("                ");
+                    for(int a = 0; a < rTable.outwardIP.size();a++) {
+                        for(int i = 0; i < rTable.outwardIP.get(a).size(); i++) {
+                            System.out.print(rTable.outwardIP.get(a).get(i).getIP() + "," + rTable.outwardIP.get(a).get(i).getPort() + "||");
+                        }
+                    }
+                    System.out.println();
+
+                    for(int i = 0; i < rTable.costToGet.size(); i++) {
+                        System.out.print(rTable.neighborAddresses.get(i).getIP() + "," + rTable.neighborAddresses.get(i).getPort() + "||");
+                        for(int j = 0; j < rTable.costToGet.get(i).size(); j++) {
+                            System.out.print(rTable.costToGet.get(i).get(j) + "               ");
+                        }
+                        System.out.println();
+                    }                
+                } else if(line.contains("CHANGE")) {
+
+                } else {
+                    System.out.println("Command does not exist");
+                    System.out.println("akjdkas");
+                }
+
+                try {
+                    line = reader.readLine();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
         }
     }
-
 }
--- root/DVThread.java
+++ root/DVThread.java
@@ -1,17 +1,22 @@
 import java.util.Timer;
+import java.io.*;
+import java.net.*;
+
 public class DVThread implements Runnable {
     TimerSendDVTask dvTask;
     RoutingTable rTable;
     Integer portNum;
-    
-    public DVThread(RoutingTable rTable, Integer portNum) {
-        dvTask = new TimerSendDVTask(rTable, portNum);
+    DatagramSocket serverSocket;
+    public DVThread(RoutingTable rTable, Integer portNum, DatagramSocket serverSocket) {
+        dvTask = new TimerSendDVTask(rTable, portNum, serverSocket);
         this.rTable = rTable;
         this.portNum = portNum;
+        this.serverSocket = serverSocket;
     }
-    
+
     public void run() {
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(dvTask,0,1000);
+        timer.scheduleAtFixedRate(dvTask,1000,5000);
     }
+
 }

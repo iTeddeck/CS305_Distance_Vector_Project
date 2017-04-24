356eb83080a0378d30f7b0b06732d84ba1c8600e
minahanjlaptop <minahanj@lafayette.edu>
2017-04-24 12:58:55 -0400

    trying to read DV from message sent, inetAddress->string gives /IP, will have to parse strings to get in correct form I think

--- root/ListenerThread.java
+++ root/ListenerThread.java
@@ -1,14 +1,17 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class ListenerThread implements Runnable {
     private int portNum;
     DatagramSocket serverSocket;
     byte[] receiveData = new byte[1024];
+    RoutingTable rTable;
     public ListenerThread(int portNum, RoutingTable rTable, DatagramSocket serverSocket) {
         this.portNum = portNum;
         //Create UDP Connection
         this.serverSocket = serverSocket;
+        this.rTable = rTable;
     }
 
     public void run() {
@@ -19,11 +22,74 @@ public class ListenerThread implements Runnable {
             int port = receivePacket.getPort();
 
             String message = new String(receivePacket.getData());
-
+            System.out.println(message);
             if(!message.equals("")) {
+                String[] messageArray = message.split(" ");
+
+                if(messageArray[0].equals("[1]")) {
+                    parseReceivedDV(messageArray, IPAddress, port);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void parseReceivedDV(String[] distanceVector, InetAddress ipFrom, int portFrom) {
+        String ipFromString = ipFrom.toString();
+        String portFromString = Integer.toString(portFrom);
+        String returnString =  "new dv received from " + ipFromString + ":" + portFromString + " with the following distances" + "\n";
+
+        //finding index of person who sent you this DV
+        int indexOfSender = -1;
+        for(int i = 0; i < rTable.neighborAddresses.size(); i++) {
+            System.out.println(rTable.neighborAddresses.get(i).getIP() + ":" + ipFromString);
+            System.out.println(rTable.neighborAddresses.get(i).getPort() + ":" + portFromString);
+            if(rTable.neighborAddresses.get(i).getIP().equals(ipFromString) 
+            && rTable.neighborAddresses.get(i).getPort().equals(portFromString)) {
+                indexOfSender = i;
+                break;
+            }
+        }
+
+        for(int i = 1; i < distanceVector.length; i++) {
+            String[] entryComponents = distanceVector[i].split("-");
+            //entryComponents[0] == ")"
+            //entryCompo[1] == IP
+            //entry compo[2] == Port
+            //entry compo[3] == cost (String form)
+            //entry comp[4] == ")"
+
+            //finding the index of this component
+            
+            returnString += entryComponents[1] + ":" + entryComponents[2] + " " + entryComponents[3] + "\n";
+
+            int indexOfDVEntry = -1;
+            if(indexOfSender != -1) { //if -1, means that neighbor wasn't found in table
+                for(int k =0; k < rTable.outwardIP.get(indexOfSender).size();k++) {
+                    if(rTable.outwardIP.get(indexOfSender).get(k).getIP().equals(entryComponents[1])
+                    && rTable.outwardIP.get(indexOfSender).get(k).getPort().equals(entryComponents[2])) {
+                        indexOfDVEntry = k;
+                    }
+                }
+            } else {
+                System.out.println("Sender of DV was not found in Table");
+            }
+
+            boolean wasChange = false; //will tell whether or not there was change in the DV
+            if(indexOfDVEntry != -1) { //if -1, means DV Entry wasn't found in neighbors outward ip table
+                if(rTable.costToGet.get(indexOfSender).get(indexOfDVEntry) == Integer.parseInt(entryComponents[3])) {
+                    //Might need to do something??
+                } else {
+                    wasChange = true;
+                }
+            }
+            
+            if(wasChange) {
+                System.out.println("DV Received from neighbor needs to be updated");
+            } 
+        }
+        
+        System.out.println(returnString);
     }
 }
--- root/TimerSendDVTask.java
+++ root/TimerSendDVTask.java
@@ -16,19 +16,21 @@ public class TimerSendDVTask extends TimerTask {
     public void run() {
         String distanceVector = ""; //will eventually be in form of <(IP,Port,Cost),(IP,Port,Cost)...>
         for(int a = 0; a < rTable.outwardIP.size(); a++) {
+            distanceVector += "[1] "; //[1] will correspond with routine DV message
             for(int i = 0; i < rTable.outwardIP.get(a).size(); i++) {
-                distanceVector += "(";
+                distanceVector += "(-";
                 distanceVector += rTable.outwardIP.get(a).get(i).getIP();
-                distanceVector += ",";
+                distanceVector += "-";
                 distanceVector += rTable.outwardIP.get(a).get(i).getPort();
-                distanceVector += ",";
+                distanceVector += "-";
                 distanceVector += rTable.costToGet.get(a).get(i);
-                distanceVector += ")";
+                distanceVector += "-) "; //spaces to delim
             }
         }
         sendData = distanceVector.getBytes();
         for(int i = 1; i < rTable.neighborAddresses.size();i++) { //0 is you
             System.out.println("Sending a message");
+            System.out.println(distanceVector);
             try {
                 InetAddress ip = InetAddress.getByName(rTable.neighborAddresses.get(i).getIP());
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, Integer.parseInt(rTable.neighborAddresses.get(i).getPort()));
